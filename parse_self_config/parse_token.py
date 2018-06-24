b6196c689fb977396f1d845843b08931e2bdde53
wangbiao1 <wangbiao1@xiaomi.com>
2018-06-24 15:41:18 +0800

    测试修改

--- parse_self_config/parse_token.py
+++ parse_self_config/parse_token.py
@@ -3,7 +3,7 @@ from get_token import SemanticParser
 if __name__ == '__main__':
     g = SemanticParser()
     # g.parse_expr_by_token("root=magic([a | aaabb | aaabbcc | cc | <a>], a, sddd)")
-    g.parse_file("arith.expr")
-    ans = g.parse("1.323234加2乘3")
+    g.parse_file("weather.expr")
+    ans = g.parse("今天天气怎么样啦")
     for i in ans:
         print( i)
